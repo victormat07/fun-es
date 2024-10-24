162cd1499b276266e5c942a7532c03331affa952
matheus <102987008+victormat07@users.noreply.github.com>
2024-10-24 10:24:37 -0300

    Update commun.js

--- graficos/commun.js
+++ graficos/commun.js
@@ -1,12 +1,12 @@
 const getCSS = (variavel) => {
-    return getComputedStyle(document.body)
-    getPropertyValue(variavel)
-  }
-  
-  const tickConfig = {
-    color: getCSS('--primary-color'),
+    const bodyStyles = getComputedStyle(document.body)
+    return bodyStyles.getPropertyValue(variavel)
+}
+
+const tickConfig = {
+    family: getCSS('--font'),
     size: 16,
-    family: getCSS('--font')
-  }
-  
-  export { getCSS, tickConfig }
+    color: getCSS('--primary-color')
+}
+
+export {getCSS, tickConfig}
